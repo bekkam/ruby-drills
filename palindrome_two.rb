6d45b930af3a56f4b815413436e829e1d6fe0664
U-Lenovo-PC\rmurphy <bekkam@users.noreply.github.com>
2016-07-30 11:56:40 -0700

    Add palindrome_two solutions

---
+++ palindrome_two.rb
@@ -0,0 +1,29 @@
+=begin
+  
+Have the function PalindromeTwo(str) take the str parameter being passed and 
+return the string true if the parameter is a palindrome, (the string is the 
+same forward as it is backward) otherwise return the string false. The 
+parameter entered may have punctuation and symbols but they should not affect 
+whether the string is in fact a palindrome. For example: "Anne, I vote more 
+cars race Rome-to-Vienna" should return true.   
+=end
+
+# Solution
+def PalindromeTwo(str)
+  str.downcase.scan(/[a-z]/) == str.downcase.scan(/[a-z]/).reverse 
+end
+
+
+# Solution
+def PalindromeTwo(str)
+  letters = str.downcase.scan(/[a-z]/).join
+  letters == letters.reverse
+end
+
+# Solution
+def PalindromeTwo(str)
+    str.downcase!
+    str.gsub!(/[\W]/, "")
+    str == str.reverse
+         
+end
